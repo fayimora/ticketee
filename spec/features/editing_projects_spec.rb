f06adeaf33de76baaa4fef16a065a433862c2929
Fayimora Femi-Balogun <fayi@fayimora.com>
2013-03-10 16:17:40 +0000

    Add updating projects functionality

---
+++ spec/features/editing_projects_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper.rb'
+
+feature "Editing projects" do
+  before do
+    FactoryGirl.create(:project, name: "TextMate 2")
+    visit '/'
+    click_link "TextMate 2"
+    click_link "Edit Project"
+  end
+
+  scenario "Updating a project" do
+    fill_in "Name", with: "TextMate 2 beta"
+    click_button "Update Project"
+    expect(page).to have_content("Project has been updated")
+  end
+
+  scenario "updating project with invalid attributes" do
+    fill_in "Name", with: ""
+    click_button "Update Project"
+    expect(page).to have_content("Project has not been updated")
+  end
+end
