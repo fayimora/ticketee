e2dda0f6ab98ade67fe4754e079a13e2b2345bd2
Fayimora Femi-Balogun <fayi@fayimora.com>
2013-05-12 08:13:28 +0100

    Add unit test for ticket model

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe Ticket do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @ticket = Ticket.new(:title => "My Ticket", :description => "Its description")
+  end
+  subject{@ticket}
+
+  it{should respond_to(:title)}
+  it{should respond_to(:description)}
 end
