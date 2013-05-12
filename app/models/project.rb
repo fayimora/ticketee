a5beb5d3e133455212b653d289617c8d41007d28
Fayimora Femi-Balogun <fayi@fayimora.com>
2013-05-12 08:08:24 +0100

    Implement edit action for the tickets controller

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,5 +2,8 @@ class Project < ActiveRecord::Base
   attr_accessible :description, :name
   validates :name, presence: true
 
-  has_many :tickets
+  # use :nullify to disassociate tickets from a project without deleting them,
+  # use :delete_all to delete just the tickets and not it's dependencies/callbacks
+  # use :destroy if you have callbacks to run on tickets when they are destroyed
+  has_many :tickets, :dependent => :delete_all
 end
