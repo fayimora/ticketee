c659d220bc81835a0494311727e3a9ac5114aab4
Fayimora Femi-Balogun <fayi@fayimora.com>
2013-05-28 19:55:18 +0100

    Add tests for user model. Closes #1

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = User.new(email: 'x@example.com', password: 'password', password_confirmation: 'password')
+  end
+  subject{@user}
+
+  it{should respond_to :email}
+  it{should respond_to :password}
+  it{should respond_to :password_confirmation}
 end
